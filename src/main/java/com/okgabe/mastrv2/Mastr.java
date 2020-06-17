cfc2e383722dc7d3d6ceb2fc6557a7cab737824d
Gabe <gabrieljameskeller@gmail.com>
2020-06-16 22:39:44 -0500

    Initial commit

---
+++ src/main/java/com/okgabe/mastrv2/Mastr.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020 Gabriel Keller
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * A copy of this license can be found at
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.
+ */
+
+package com.okgabe.mastrv2;
+
+public class Mastr {
+    
+    public static void main(String[] args) {
+    }
+}
