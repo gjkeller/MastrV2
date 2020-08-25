c8ddb0ae895988fe2710eac241edb769bf3572f1
Gabe <gabrieljameskeller@gmail.com>
2020-08-25 00:52:59 -0500

    ReflectionUtil changes to allow searching while packaged in jar

--- src/main/java/com/okgabe/mastr2/util/ReflectionUtil.java
+++ src/main/java/com/okgabe/mastr2/util/ReflectionUtil.java
@@ -8,66 +8,85 @@
 
 package com.okgabe.mastr2.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import com.okgabe.mastr2.command.CommandBase;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class ReflectionUtil {
-    /**
-     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
-     *
-     * @param packageName The base package
-     * @return The classes
-     * @throws ClassNotFoundException
-     * @throws IOException
-     *
-     * From https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection
-     */
-    public static Class[] getClasses(String packageName)
-            throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        ArrayList<Class> classes = new ArrayList<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes.toArray(new Class[classes.size()]);
-    }
+//    /**
+//     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
+//     *
+//     * @param packageName The base package
+//     * @return The classes
+//     * @throws ClassNotFoundException
+//     * @throws IOException
+//     *
+//     * From https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection
+//     */
+//    public static Class[] getClasses(String packageName)
+//            throws ClassNotFoundException, IOException {
+//        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//        assert classLoader != null;
+//        String path = packageName.replace('.', '/');
+//        Enumeration<URL> resources = classLoader.getResources(path);
+//        List<File> dirs = new ArrayList<>();
+//        while (resources.hasMoreElements()) {
+//            URL resource = resources.nextElement();
+//            dirs.add(new File(resource.getFile()));
+//        }
+//        ArrayList<Class> classes = new ArrayList<>();
+//        for (File directory : dirs) {
+//            classes.addAll(findClasses(directory, packageName));
+//        }
+//        return classes.toArray(new Class[classes.size()]);
+//    }
+//
+//    /**
+//     * Recursive method used to find all classes in a given directory and subdirs.
+//     *
+//     * @param directory   The base directory
+//     * @param packageName The package name for classes found inside the base directory
+//     * @return The classes
+//     * @throws ClassNotFoundException
+//     *
+//     * From https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection
+//     */
+//    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+//        List<Class> classes = new ArrayList<>();
+//        if (!directory.exists()) {
+//            return classes;
+//        }
+//        File[] files = directory.listFiles();
+//        for (File file : files) {
+//            if (file.isDirectory()) {
+//                assert !file.getName().contains(".");
+//                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+//            } else if (file.getName().endsWith(".class")) {
+//                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+//            }
+//        }
+//        return classes;
+//    }
 
-    /**
-     * Recursive method used to find all classes in a given directory and subdirs.
-     *
-     * @param directory   The base directory
-     * @param packageName The package name for classes found inside the base directory
-     * @return The classes
-     * @throws ClassNotFoundException
-     *
-     * From https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection
-     */
-    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-            }
-        }
-        return classes;
+    // From https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection
+    public static Set<Class<? extends CommandBase>> getClasses(String packageName){
+        List<ClassLoader> classLoadersList = new LinkedList<>();
+        classLoadersList.add(ClasspathHelper.contextClassLoader());
+        classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageName))));
+
+        return reflections.getSubTypesOf(CommandBase.class);
     }
 }
